995f7066c0fdfdaea2500e315a4912ea0af31a31
Dhayanandh <dhayanandh14@gmail.com>
2021-08-10 00:40:42 +0530

    remove white space from email and name

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ require "csv"
 students = CSV.parse(File.read("Students_list_batch2.csv"), encoding: "UTF-8", headers: true)
 students.each do |student|
   if student["name"] && student["email"] && student["dob"]
-    Applicant.create!(name: student["name"], email: student["email"], dob: Date.strptime(student["dob"], "%m/%d/%Y"))
+    Applicant.create!(name: student["name"].strip, email: student["email"].strip, dob: Date.strptime(student["dob"], "%m/%d/%Y"))
   end
 end
