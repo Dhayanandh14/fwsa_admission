c5266e1df1460fe0076e7d54004083a0cb1aac39
Dhayanandh <dhayanandh14@gmail.com>
2021-08-09 23:36:14 +0530

    remove duplicate entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 students = CSV.parse(File.read("Students_list_batch2.csv"), encoding: "UTF-8", headers: true)
-students.each do |student|
+students.uniq.each do |student|
   if student["name"] && student["email"] && student["dob"]
     Applicant.create!(name: student["name"], email: student["email"], dob: Date.strptime(student["dob"], "%m/%d/%Y"))
   end
