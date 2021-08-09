e67d45ec34df178123da7969e781f5c7eb231324
Dhayanandh <dhayanandh14@gmail.com>
2021-08-09 23:50:37 +0530

    remove validation from model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 students = CSV.parse(File.read("Students_list_batch2.csv"), encoding: "UTF-8", headers: true)
-students.uniq.each do |student|
+students.each do |student|
   if student["name"] && student["email"] && student["dob"]
     Applicant.create!(name: student["name"], email: student["email"], dob: Date.strptime(student["dob"], "%m/%d/%Y"))
   end
