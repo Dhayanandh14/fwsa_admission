2b241d5c63d34ffb288caa2d2f5dfd0293533f87
Dhayanandh <dhayanandh14@gmail.com>
2021-08-09 22:54:00 +0530

    add seeds

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,4 @@
 class Applicant < ApplicationRecord
-  validates :name,uniqueness:{scope: :email}
 
   def eligible?
     dob.year.to_i >= 2000 && dob.year.to_i <= 2003
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+students = CSV.parse(File.read("Students_list_batch2.csv"), encoding: "UTF-8", headers: true)
+students.each do |student|
+  if student["name"] && student["email"] && student["dob"]
+    Applicant.create!(name: student["name"], email: student["email"], dob: Date.strptime(student["dob"], "%m/%d/%Y"))
+  end
+end
