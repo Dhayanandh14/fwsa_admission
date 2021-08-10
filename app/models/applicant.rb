1096f532edd8e39306d52f5499c860eb9cb693ff
Dhayanandh14 <dhayanandh14@gmail.com>
2021-08-10 20:46:46 +0530

    Add details page and show the details

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,5 @@
 class Applicant < ApplicationRecord
   def eligible?
-    dob.year.to_i >= 2000 && dob.year.to_i <= 2003
+    dob.year.to_i >= 2000 && dob.year.to_i <= 2004
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'applicants#index'
   get 'applicant' => 'applicants#show'
+  get 'details' => 'details#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
