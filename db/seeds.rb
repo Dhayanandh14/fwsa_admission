21039ebf346f182189d0ab7c3b42b102e2a49532
Dhayanandh14 <dhayanandh14@gmail.com>
2021-08-11 13:03:54 +0530

    Add new students list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "csv"
-students = CSV.parse(File.read("Students_list_batch2.csv"), encoding: "UTF-8", headers: true)
+students = CSV.parse(File.read("Students_list_batch2_Sheet2.csv"), encoding: "UTF-8", headers: true)
 students.each do |student|
   if student["name"] && student["email"] && student["dob"]
     Applicant.create!(name: student["name"].strip, email: student["email"].strip.downcase, dob: Date.strptime(student["dob"], "%m/%d/%Y"))
