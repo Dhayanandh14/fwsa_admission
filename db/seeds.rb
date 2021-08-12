0faae402acba30e5ed57112e3500e959e1dfd766
Dhayanandh14 <dhayanandh14@gmail.com>
2021-08-12 17:51:06 +0530

    rename the file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "csv"
-file_names = ["Students_list_batch2.csv","Students_list_batch2_sheet2.csv"]
+file_names = ["Students_list_batch2.csv","Students_list_batch2_Sheet2.csv"]
 file_names.each do |file_name|
   students = CSV.parse(File.read(file_name), encoding: "UTF-8", headers: true)
   students.each do |student|
