1973b711a7df2a52b02d88fb9061bd487f69ff8c
Dhayanandh14 <dhayanandh14@gmail.com>
2021-08-13 21:12:23 +0530

    Add student_list_batch2_sheet3.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require "csv"
-file_names = ["Students_list_batch2.csv","Students_list_batch2_Sheet2.csv"]
-file_names.each do |file_name|
-  students = CSV.parse(File.read(file_name), encoding: "UTF-8", headers: true)
+# file_names = ["Students_list_batch2.csv","Students_list_batch2_Sheet2.csv"]
+file_names ="Students_list_batch2_Sheet3.csv"
+# file_names.each do |file_name|
+  students = CSV.parse(File.read("Students_list_batch2_Sheet3.csv"), encoding: "UTF-8", headers: true)
   students.each do |student|
     if student["name"] && student["email"] && student["dob"]
       Applicant.create_bundle_data(student["name"],student["email"],student["dob"])
     end
   end
-end
+# end
