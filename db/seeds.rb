cde9607ec7a33bb7d64748c24b04a10793554da0
Dhayanandh Parthiban <dhparthiban@FWS-CHE-LT-6187.local>
2021-09-01 20:17:23 +0530

    comment the status update lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
-require "csv"
-results ="batch2_students_results.csv"
-# results.each do |result|
-  student_results = CSV.parse(File.read(results), encoding: "UTF-8")
-  student_results.each do |student|
-    puts student
-    if student
-        Applicant.create_bundle_data(student)
-      end
-   end
-
-
-
-
-
-
 # require "csv"
-# file_names = ["Students_list_batch2.csv","Students_list_batch2_Sheet2.csv","Students_list_batch2_Sheet3.csv"]
-# file_names.each do |file_name|
-#   students = CSV.parse(File.read(file_name), encoding: "UTF-8", headers: true)
-#   students.each do |student|
-#     if student["name"] && student["email"] && student["dob"]
-#       Applicant.create_bundle_data(student["name"],student["email"],student["dob"])
-#     end
-#   end
-# end
+# results ="batch2_students_results.csv"
+# # results.each do |result|
+#   student_results = CSV.parse(File.read(results), encoding: "UTF-8")
+#   student_results.each do |student|
+#     puts student
+#     if student
+#         Applicant.create_bundle_data(student)
+#       end
+#    end
+
+
+
+
+
+
+require "csv"
+file_names = ["Students_list_batch2.csv","Students_list_batch2_Sheet2.csv","Students_list_batch2_Sheet3.csv"]
+file_names.each do |file_name|
+  students = CSV.parse(File.read(file_name), encoding: "UTF-8", headers: true)
+  students.each do |student|
+    if student["name"] && student["email"] && student["dob"]
+      Applicant.create_bundle_data(student["name"],student["email"],student["dob"])
+    end
+  end
+end
